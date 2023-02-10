548536cc9ec755e2881b174e07991076afc9aeeb
xAn4ss <13anas.oukharta37@gmail.com>
2023-02-11 00:26:33 +0100

    modifs

--- Sources/move_manager.c
+++ Sources/move_manager.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   move_manager.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: an4ss <an4ss@student.42.fr>                +#+  +:+       +#+        */
+/*   By: aoukhart <aoukhart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 18:39:00 by an4ss             #+#    #+#             */
-/*   Updated: 2023/02/10 12:19:51 by an4ss            ###   ########.fr       */
+/*   Updated: 2023/02/10 23:31:35 by aoukhart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,50 @@ void	handle_move_x(t_data *data)
 
 	if (data->sh.move_x == -1)
 	{
-		// new_x = ;
-		// new_y = ;
-		if (data->map[(int)(data->sh.y + 10*round(sin(-data->sh.angle - (M_PI / 2)))) / 42][(int)(data->sh.x + 10*round(cos(-data->sh.angle - (M_PI / 2))))/ 42] != '1')
+		new_x = round(cos(-data->sh.angle - (M_PI / 2)));
+		new_y = round(sin(-data->sh.angle - (M_PI / 2)));
+		if (data->map[(int)(data->sh.y + (new_y * 15)) / 42][(int)(data->sh.x + (new_x * 15))/ 42] != '1')
 		{
-			data->sh.x = data->sh.x + (cos(-data->sh.angle - (M_PI / 2)));
-			data->sh.y = data->sh.y + (sin(-data->sh.angle - (M_PI / 2)));
+			data->sh.x += new_x;
+			data->sh.y += new_y;
 		}
 	}
 	else if (data->sh.move_x == 1)
 	{
-		if (data->map[(int)(data->sh.y - 10*round(sin(-data->sh.angle - (M_PI / 2)))) / 42][(int)(data->sh.x - 10*round(cos(-data->sh.angle - (M_PI / 2))))/ 42] != '1')
+		if (data->map[(int)(data->sh.y - (new_y * 15)) / 42][(int)(data->sh.x - (new_x * 15))/ 42] != '1')
 		{
-			data->sh.x = data->sh.x - (cos(-data->sh.angle - (M_PI / 2)));
-			data->sh.y = data->sh.y - (sin(-data->sh.angle - (M_PI / 2)));
+			data->sh.x -= new_x;
+			data->sh.y -= new_y;
 		}
 	}
 }
 
 void	handle_move_y(t_data *data)
 {
-	int	new_x;
-	int	new_y;
+	double new_x;
+	double new_y;
 
 	if (data->sh.move_y == -1)
 	{
-		if (data->map[(int)(data->sh.y + 10*round(sin(-data->sh.angle))) / 42][(int)(data->sh.x + 10*round(cos(-data->sh.angle))) / 42] != '1')
+		new_x = round(cos(-data->sh.angle));
+		new_y = round(sin(-data->sh.angle));
+		if (data->map[(int)(data->sh.y + (new_y * 15)) / 42][(int)(data->sh.x + (new_x * 15)) / 42] != '1')
 		{
-			data->sh.x += cos(-data->sh.angle)*2;
-			data->sh.y += sin(-data->sh.angle)*2;
+			data->sh.x += new_x;
+			data->sh.y += new_y;
 		}
+		// if (data->map[(int)(data->sh.y + (new_y * 15)) / 42][(int)data->sh.x / 42] != '1')
 	}
 	if (data->sh.move_y == 1)
 	{
-		if (data->map[(int)(data->sh.y + 10*round(sin(-data->sh.angle + M_PI))) / 42][(int)(data->sh.x + 10*round(cos(-data->sh.angle + M_PI))) / 42] != '1')
+		new_x = round(cos(-data->sh.angle + M_PI));
+		new_y = round(sin(-data->sh.angle + M_PI));
+		if (data->map[(int)(data->sh.y + (new_y * 15)) / 42][(int)(data->sh.x + (new_x * 15)) / 42] != '1')
 		{
-			data->sh.x += cos(-data->sh.angle + M_PI)*2;
-			data->sh.y += sin(-data->sh.angle + M_PI)*2;
+			data->sh.x += new_x;
+			data->sh.y += new_y;
 		}
+		// if (data->map[(int)(data->sh.y + (new_y * 15)) / 42][(int)data->sh.x / 42] != '1')
 	}
 }
 
@@ -99,19 +105,19 @@ int	release_key(int num, t_data *data)
 
 int	update(t_data *data)
 {
-	// if (data->i < 100)
-	// {
-	// 	data->i++;
-	// 	return (1);
-	// }
-	// else if (data->i == 100)
-	// 	data->i = 0;
+	if (data->i < 100)
+	{
+		data->i++;
+		return (1);
+	}
+	else if (data->i == 100)
+		data->i = 0;
 	handle_move_x(data);
 	handle_move_y(data);
 	if (data->sh.rot)
-		data->sh.angle -= 3 * (M_PI / 180) * data->sh.rot;
+		data->sh.angle -= 10 * (M_PI / 180) * data->sh.rot;
 	mlx_clear_window(data->mlx, data->win);
 	render_walls(data);
-	// data->i++;
+	data->i++;
 	return (1);
 }
--- cub.h
+++ cub.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: an4ss <an4ss@student.42.fr>                +#+  +:+       +#+        */
+/*   By: aoukhart <aoukhart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 17:21:21 by ybachaki          #+#    #+#             */
-/*   Updated: 2023/02/10 10:31:43 by an4ss            ###   ########.fr       */
+/*   Updated: 2023/02/10 19:43:28 by aoukhart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,24 @@
 # include<ctype.h>
 # include"get_next_line.h"
 # include"libft.h"
-# include"../minilibx-linux-master/mlx.h"
+# include"mlx.h"
 // #include <X11/keysym.h>
 # define SCREEN_W 1080
 # define SCREEN_H 720
-// # define LEFT 0
-# define LEFT 113
-# define UP 122
-// # define UP 13
-// # define RIGHT 2/*100*/
-# define RIGHT 100
-# define DOWN 115
-// # define DOWN 1/*1
-// # define ECHAP 53/*65307*/
-# define ECHAP 65307
-// # define ROT_LEFT 123/*65361*/
-# define ROT_LEFT 65361
-// # define ROT_RIGHT 124/*65363*/
-# define ROT_RIGHT 65363
+# define LEFT 0
+// # define LEFT 113
+// # define UP 122
+# define UP 13
+# define RIGHT 2/*100*/
+// # define RIGHT 100
+// # define DOWN 115
+# define DOWN 1/*1*/
+# define ECHAP 53/*65307*/
+// # define ECHAP 65307
+# define ROT_LEFT 123/*65361*/
+// # define ROT_LEFT 65361
+# define ROT_RIGHT 124/*65363*/
+// # define ROT_RIGHT 65363
 # define MOVE 10
 # define CHECK 20
 
